7c7a3d4be11f14e91f88d6059067c8926174beed
aka147 <ajajtajpgj@gmail.com>
2020-12-16 15:16:58 +0000

    3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = 'e93fdbc4d730cc4d6ef6b8d5dfa673649034e615edf237e060211ea638069fd7ea114e248ec68ec8c2b774d010a413f7392fd483e00f601f36c9d30f76c50e15'
